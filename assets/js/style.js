39ee815e6603717129703355d7db8f6d0cf77893
quangnd512 <quangnd512@gmail.com>
2022-08-24 17:13:05 +0700

    add code footer ngay 24/8

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,6 +1,7 @@
 // Accordion footer
 $( function() {
-    $( "#accordion" ).accordion();
+    $("#accordion").accordion();
+    $('#ui-id-2, #ui-id-4, #ui-id-6').css('height', 'auto')
 } );
 // Accordion footer
 
